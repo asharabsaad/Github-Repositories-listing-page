9e175afae6cb9a18ac64430c81406ad8a78ad918
Asharab Saad <104360135+asharabsaad@users.noreply.github.com>
2024-01-19 14:50:45 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+// Octokit.js
+// https://github.com/octokit/core.js#readme
+const octokit = new Octokit({
+    auth: 'YOUR-TOKEN'
+  })
+  
+  await octokit.request('GET /orgs/{org}/repos', {
+    org: 'ORG',
+    headers: {
+      'X-GitHub-Api-Version': '2022-11-28'
+    }
+  })
